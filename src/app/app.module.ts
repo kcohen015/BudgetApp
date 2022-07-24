6b51a56eab2a2773440d868a2b3a42c176bc2e60
Kathryn Cohen <kcohen@xpanxion.com>
2022-07-24 17:49:49 -0400

    add item cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { AddItemFormComponent } from './components/add-item-form/add-item-form.component';
+import { BudgetItemListComponent } from './components/budget-item-list/budget-item-list.component';
+import { BudgetItemCardComponent } from './components/budget-item-list/budget-item-card/budget-item-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    AddItemFormComponent,
+    BudgetItemListComponent,
+    BudgetItemCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/add-item-form/add-item-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import { BudgetItem } from 'src/shared/models/budget-item.model';
+
+@Component({
+  selector: 'app-add-item-form',
+  templateUrl: './add-item-form.component.html',
+  styleUrls: ['./add-item-form.component.scss']
+})
+export class AddItemFormComponent implements OnInit {
+
+  @Input() item: BudgetItem = new BudgetItem('', 0);
+
+  @Output() formSubmit: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm){
+    this.formSubmit.emit(form.value);
+  }
+}
